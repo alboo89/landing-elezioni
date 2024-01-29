055be0425df511bdcfa09294cdf37040a61dfea7
Alberto <Piras>
2024-01-29 13:35:55 +0100

    Update favicon and social media images
    Add Luciana Mele's information to index.html
    
    Update Home.tsx with social media buttons and translations
    
    Update common.json with translations for home page

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,15 +1,21 @@
 import {
+  Box,
   Button,
+  ButtonGroup,
   Card,
-  CardActions,
   CardContent,
   CardMedia,
   Container,
+  IconButton,
+  Stack,
   Typography,
 } from '@mui/material';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import InstagramIcon from '@mui/icons-material/Instagram';
 import lucianaBG from '../../assets/luciana.png';
 import { ThemedLayoutV2 } from '@refinedev/mui';
 import { Header } from '../../components/header';
+import { useTranslate } from '@refinedev/core';
 
 const Home: React.FC = () => (
   <Container sx={{ p: '0 !important' }} maxWidth={false}>
@@ -25,6 +31,7 @@ const Home: React.FC = () => (
 export default Home;
 
 const MediaCard = () => {
+  const translate = useTranslate();
   return (
     <Card sx={{ height: '100%', maxWidth: '100%' }}>
       <CardContent>
@@ -33,16 +40,39 @@ const MediaCard = () => {
           variant="subtitle1"
           color="red"
         >
-          SITO WEB IN COSTRUZIONE
+          {translate('page.home.wip')}
         </Typography>
-        <Typography gutterBottom variant="h3" component="div">
-          Luciana Mele
-        </Typography>
+        <Stack sx={{ mb: 2, textAlign: 'center' }} direction="row" spacing={5}>
+          <Typography gutterBottom variant="h3" component="div">
+            Luciana Mele
+          </Typography>
+          <Box alignSelf="center" height="100%">
+            <ButtonGroup>
+              <Button
+                variant="outlined"
+                size="medium"
+                href="https://www.facebook.com/luciana.mele.756"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <FacebookIcon />
+              </Button>
+              <Button
+                variant="outlined"
+                size="medium"
+                href="https://www.instagram.com/lucianamele_62"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <InstagramIcon />
+              </Button>
+            </ButtonGroup>
+          </Box>
+        </Stack>
         <Typography variant="h4" color="secondary">
-          Candidata alla carica di consigliere della Regione Sardegna
+          {translate('page.home.subtitle1')}
           <Typography variant="subtitle1" color="primary">
-            Elezioni del 25 febbraio 2024, lista Vota Sardigna / Coalizione
-            Sarda / Renato Soru Presidente
+            {translate('page.home.subtitle2')}
           </Typography>
         </Typography>
       </CardContent>
@@ -51,11 +81,6 @@ const MediaCard = () => {
         image={lucianaBG}
         title="Vota Luciana Mele!"
       />
-
-      <CardActions>
-        <Button size="small">Share</Button>
-        <Button size="small">Learn More</Button>
-      </CardActions>
     </Card>
   );
 };
